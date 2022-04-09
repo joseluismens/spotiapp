1e8349df74c0d01eb9d36871aa3fa9379df1a4a1
Jose Luis Muñoz <joseluismunozzuniga@gmail.com>
2022-04-09 19:52:08 -0400

    terminao

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,13 +12,19 @@ export class HomeComponent implements OnInit {
   nuevasCanciones: any[] = [];
   loading: boolean;
   error:boolean;
+  mensaje:string='';
   constructor(private spotifyService: SpotifyService) {
     this.loading = true;
     this.error=false;
     this.spotifyService.getNewReleases().subscribe(
       (data: any) => { this.nuevasCanciones = data; this.loading = false },
-      (error)=>{
+      (error:any)=>{
         console.log(error);
+        this.error=true;
+        this.loading=false;
+        this.mensaje = error.error.error.message;
+        console.log(this.mensaje);
+        
         
       })
 
